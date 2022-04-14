c40a7b81aee9564278297a1fcaac762facfd08c9
Adam Ližičiar <adamliziciar27@gmail.com>
2022-04-14 15:29:24 +0200

    Main structure

---
+++ proj2.c
@@ -0,0 +1,11 @@
+/************************************************/
+/*	    Author: Adam Ližičiar (xlizic00)	    */
+/************************************************/
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main (int argc, char *argv[])
+{
+    return 0; 
+}
